0b63323c9949dc920d82352c33db51ee4b8b0513
magdalenamoeller <magdalena.moeller@awin.com>
2018-03-02 10:59:43 +0100

    SequenceProducer

---
+++ src/main/java/com/zanox/SequenceProducer.java
@@ -0,0 +1,66 @@
+package com.zanox;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequenceProducer {
+    public static void main(String args[]) {
+        String topic = "test"; //"args[0];
+        String broker = "172.17.0.1:9092";//"localhost:9092";//"172.19.0.1:9092"; //args[1];
+
+        new SequenceProducer(topic, broker);
+    }
+
+    private SequenceProducer(String topic, String broker) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        AtomicInteger counter = new AtomicInteger(1);
+        executor.submit((Runnable) () -> {
+            KafkaProducer<String, String> producer = null;
+            // Loop forever
+            while (true) {
+                boolean done = false;
+                String current = String.valueOf(counter.getAndIncrement());
+                do {
+                    if (producer == null) {
+                        producer = makeProducer(topic, broker);
+                    }
+                    try {
+                        RecordMetadata m = producer.send(new ProducerRecord<>(
+                                topic, current, current)
+                        ).get();
+                        System.err.println(String.format(
+                                "%s - %d [%d] %s",
+                                m.topic(), m.partition(), m.offset(), current
+                        ));
+                        done = true;
+                    } catch (Exception e) {
+                        producer.close();
+                        producer = null;
+                        System.err.println(e);
+                    }
+                } while (!done);
+            }
+        });
+    }
+
+    private static KafkaProducer<String, String> makeProducer(String topic, String broker) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", broker);
+        props.put("acks", "all");
+        props.put("retries", Integer.MAX_VALUE);
+        props.put("batch.size", 1);
+        props.put("linger.ms", 0);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        return producer;
+    }
+}
